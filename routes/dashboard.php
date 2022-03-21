580e6de62b660284607acad236d3846e71d5083b
level7up <abdoleemos200@gmail.com>
2022-03-22 00:13:36 +0200

    a

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -13,17 +12,3 @@ Route::post('/dashboard/login', [LoginController::class, 'login']);
 
 
 Route::get('/home', [DashboardController::class , 'index'])->name('dashboard');
-=======
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Level7up\Dashboard\Http\Controllers\DashboardController;
-use Level7up\Dashboard\Http\Controllers\Auth\LoginController;
-
- // --------------- AUTH ----------------
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('dashboard.logout');
-
-Route::get('/home', [DashboardController::class , 'index'])->name('home');
->>>>>>> 33d6f31f975fcc4b26055cda5adde90a09f46d5b
--- src/Providers/DashboardServiceProvider.php
+++ src/Providers/DashboardServiceProvider.php
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Level7up\Dashboard\Providers;
-
-use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\ServiceProvider;
-use Level7up\Dashboard\Console\CreateAdmin;
-
-class DashboardServiceProvider extends ServiceProvider
-{
-
-    public function boot()
-    {
-        $this->loadRoutesFrom(__DIR__.'/../../routes/dashboard.php');
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'dashboard');
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-        $this->loadTranslationsFrom(__DIR__.'/../../resources/lang', 'dashboard');
-        $this->registerRoutes();
-        if ($this->app->runningInConsole()) {
-            $this->registerCommands();
-            // $this->registerCommands();
-        }
-        $this->publishes([
-            __DIR__ . '/../../public/assets' => public_path('assets/dashboard'),
-            __DIR__ . '/../../public/vendor' => public_path('vendor'),
-        ], 'dashboard-assets');
-
-
-    }
-    public function register()
-    {
-        $this->mergeConfigFrom(
-            __DIR__.'/../../config/dashboard.php',
-            'dashboard'
-        );
-    }
-    private function registerRoutes()
-    {
-        Route::group([
-            'middleware' => ['web'],
-        ], function () {
-            $this->loadRoutesFrom(__DIR__.'/../../routes/dashboard.php');
-        });
-
-    }
-    private function registerCommands()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                CreateAdmin::class,
-            ]);
-        }
-    }
-
-}
-=======
 <?php
 
 namespace Level7up\Dashboard\Providers;
@@ -119,4 +61,3 @@ class DashboardServiceProvider extends ServiceProvider
         }
     }
 }
->>>>>>> 33d6f31f975fcc4b26055cda5adde90a09f46d5b

72bcb4a06692b889f2e2deca0b15250feac49936
level7up <abdoleemos200@gmail.com>
2022-03-22 19:41:36 +0200

    a

--- src/Models/Admin.php
+++ src/Models/Admin.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace Level7up\Dashboard\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-// use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-class Admin extends Authenticatable
-{
-    use HasFactory;
-
-    protected $base_route = "dashboard.users";
-
-    protected $guard_name = 'admin';
-
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-}
-=======
 <?php
 
 namespace Level7up\Dashboard\Models;
@@ -54,4 +25,3 @@ class Admin extends Authenticatable
         'remember_token',
     ];
 }
->>>>>>> 33d6f31f975fcc4b26055cda5adde90a09f46d5b

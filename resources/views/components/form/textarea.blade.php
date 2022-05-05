180e5b6dfeefd4ecaaedd6165b916917d7f80b83
level7up <abdoleemos200@gmail.com>
2022-05-06 00:19:41 +0200

    :zap: text area validate

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,14 +1,16 @@
 @props(['name', 'rows' => 6, 'placeholder' => null, 'value' => old($name)])
 
 <div class="d-flex flex-column mb-10 fv-row fv-plugins-icon-container">
-    <label class="form-label fs-6 fw-bolder text-dark"
-        for="{{ $name }}">
+    <label class="form-label fs-6 fw-bolder text-dark" for="{{ $name }}">
         {{ $slot }}
     </label>
 
-    <textarea class="form-control form-control-solid"
-        rows="{{ $rows }}"
-        name="{{ $name }}"
-        placeholder="{{ $slot }}">{{ $value }}</textarea>
+    <textarea class="form-control form-control-solid @error($name) is-invalid @enderror" rows="{{ $rows }}"
+        name="{{ $name }}" placeholder="{{ $slot }}">{{ $value }}</textarea>
     <div class="fv-plugins-message-container invalid-feedback"></div>
+    @error($name)
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
 </div>

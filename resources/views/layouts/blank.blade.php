bfd30efb8f04e55df3316fb1af26e3b6a70db2fd
level7up <abdoleemos200@gmail.com>
2022-12-07 22:09:13 +0200

    :zap:

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -2,8 +2,26 @@
 
 @include('dashboard::partials.head')
 
-<body id="kt_body" class="bg-white">
-
+<body id="kt_body" class="app-blank bgi-size-cover bgi-position-center bgi-no-repeat">
+    <script>
+        var defaultThemeMode = "light";
+        var themeMode;
+        if (document.documentElement) {
+            if (document.documentElement.hasAttribute("data-theme-mode")) {
+                themeMode = document.documentElement.getAttribute("data-theme-mode");
+            } else {
+                if (localStorage.getItem("data-theme") !== null) {
+                    themeMode = localStorage.getItem("data-theme");
+                } else {
+                    themeMode = defaultThemeMode;
+                }
+            }
+            if (themeMode === "system") {
+                themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+            }
+            document.documentElement.setAttribute("data-theme", themeMode);
+        }
+    </script>
     @yield('content')
 
     <script src="{{ asset('dashboard/plugins/global/plugins.bundle.js') }}"></script>

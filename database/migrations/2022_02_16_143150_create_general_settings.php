0488929ad03c77079a945c4e67e74aa37da11b87
level7up <abdoleemos200@gmail.com>
2023-01-11 23:11:17 +0200

    :zap:

--- database/migrations/2022_02_16_143150_create_general_settings.php
+++ database/migrations/2022_02_16_143150_create_general_settings.php
@@ -6,21 +6,21 @@ class CreateGeneralSettings extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('general.site_name','Tabani');
-        $this->migrator->add('general.email','Tabani@gmail.com');
+        $this->migrator->add('general.site_name','Level7up');
+        $this->migrator->add('general.email','Level7up@gmail.com');
         $this->migrator->add('general.phone','+201000000010');
         $this->migrator->add('general.mainColor','#35a180');
         $this->migrator->add('general.site_description',[
             'en'=>'Enjoy a smoother and easier experience on your phone, download the app now',
-            'ar' =>'وصف الموقع بس بالعربي'
+            'ar' =>'وصف الموقع  بالعربي'
         ]);
         $this->migrator->add('general.address',[
-            'en'=>'Egypt Tala',
-            'ar' =>'العنوان بس بالعربي'
+            'en'=>'Berket Elsab3',
+            'ar' =>'بركة السبع'
         ]);
         $this->migrator->add('general.slogan',[
             'en'=>'That’s it! Now it’s time to select the perfect slogan for your latest endeavor t',
-            'ar' =>'السلوجن بس  بالعربي'
+            'ar' =>'السلوجن بالعربي'
         ]);
         $this->migrator->add('general.copyrights',[
             'en'=>'Tabani copyrights',
--- database/migrations/2022_03_06_095111_social_settings.php
+++ database/migrations/2022_03_06_095111_social_settings.php
@@ -6,11 +6,11 @@ class SocialSettings extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('social.facebook', 'https://facebook.com/hash');
-        $this->migrator->add('social.twitter', 'https://twitter.com/hash');
-        $this->migrator->add('social.linkedin', 'https://linkedin.com/hash');
-        $this->migrator->add('social.instagram', 'https://instagram.com/hash');
-        $this->migrator->add('social.snapchat', 'https://snapchat.com/hash');
-        $this->migrator->add('social.tiktok', 'https://tiktok.com/hash');
+        $this->migrator->add('social.facebook', 'https://facebook.com/level7up');
+        $this->migrator->add('social.twitter', 'https://twitter.com/level7up');
+        $this->migrator->add('social.linkedin', 'https://linkedin.com/level7up');
+        $this->migrator->add('social.instagram', 'https://instagram.com/level7up');
+        $this->migrator->add('social.snapchat', 'https://snapchat.com/level7up');
+        $this->migrator->add('social.tiktok', 'https://tiktok.com/level7up');
     }
 }

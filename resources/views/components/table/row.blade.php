34038aecc49c84bb9a5274bd48be50b4a100cf1c
level7up <abdoleemos200@gmail.com>
2023-01-20 16:01:01 +0200

    :zap:

--- resources/views/components/table/row.blade.php
+++ resources/views/components/table/row.blade.php
@@ -1,6 +1,6 @@
 @props([
-    
+
 ])
-<tr @class(["text-start text-muted fw-bolder fs-7 text-uppercase gs-0"])>
+<tr @class(["text-center text-muted fw-bolder fs-7 text-uppercase gs-0"])>
     {!!$slot!!}
 </tr>
